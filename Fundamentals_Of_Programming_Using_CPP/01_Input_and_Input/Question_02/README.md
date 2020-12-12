e8dbebb0ad2fcd0d36573b6debfc247e6cbb1a93
Kamal Sharma <kamaldgrt@gmail.com>
2020-12-12 22:50:59 +0530

    Hello World With Tabs

---
+++ Fundamentals_Of_Programming_Using_CPP/01_Input_and_Input/Question_02/README.md
@@ -0,0 +1,9 @@
+# Hello World with Tab
+
+Write a program to print the given sample output.
+
+### Sample Output:
+
+```
+Hello World Hello World
+```
---
+++ Fundamentals_Of_Programming_Using_CPP/01_Input_and_Input/Question_02/question_02.cpp
@@ -0,0 +1,10 @@
+// Hello World with Tab
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World\tHello World";
+    return 0;
+}
