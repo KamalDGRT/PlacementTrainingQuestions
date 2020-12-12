300883cae935100d603491d861773b32b7091022
Kamal Sharma <kamaldgrt@gmail.com>
2020-12-12 22:54:33 +0530

    Hello World with new line

---
+++ Fundamentals_Of_Programming_Using_CPP/01_Input_and_Input/Question_03/README.md
@@ -0,0 +1,10 @@
+# Hello World with new line
+
+Write a program to print the given sample output.
+
+### SAMPLE OUTPUT:
+
+```
+Hello World
+Hello World
+```
---
+++ Fundamentals_Of_Programming_Using_CPP/01_Input_and_Input/Question_03/question_03.cpp
@@ -0,0 +1,10 @@
+// Hello World with new line
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World\nHello World";
+    return 0;
+}
