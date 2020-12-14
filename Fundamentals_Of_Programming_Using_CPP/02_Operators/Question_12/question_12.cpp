0d0910e1d439de0882ab9f4a7ed14353963ab1e3
Kamal Sharma <kamaldgrt@gmail.com>
2020-12-14 22:15:13 +0530

    Fencing the Ground

--- Fundamentals_Of_Programming_Using_CPP/02_Operators/Question_12/question_12.cpp
+++ Fundamentals_Of_Programming_Using_CPP/02_Operators/Question_12/question_12.cpp
@@ -1,2 +1,17 @@
 // Fencing the ground
 
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int l, b, p, a;
+    cin >> l >> b;
+
+    p = 2 * (l + b);
+    a = l * b;
+
+    cout << "Required length is " << p << " m" << endl;
+    cout << "Required area of carpet is " << a << " sqm";
+    return 0;
+}
